19497d356e78fb5dfab352957bdd42a680fc86b1
plcnk <romain.pluciennik@gmail.com>
2021-10-28 15:46:49 +0200

    Added k8s cluster deployment

---
+++ digitalocean-k8s-cluster/vars.tf
@@ -0,0 +1,12 @@
+# Provider variables
+variable "api_token" {}
+
+# Cluster variables
+variable "cluster_name" {}
+variable "region" {}
+variable "k8s_version" {}
+
+# Node Pool variables
+variable "pool_name" {}
+variable "size" {}
+variable "node_count" {}
